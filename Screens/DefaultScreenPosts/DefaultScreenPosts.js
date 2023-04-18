832f542fe9bdcf38894096f5b6371482d3bbeab4
Hanna Kostiana <anna.kostianaia@gmail.com>
2023-04-18 05:26:16 +0300

    last changes, part 5

--- Screens/DefaultScreenPosts/DefaultScreenPosts.js
+++ Screens/DefaultScreenPosts/DefaultScreenPosts.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from "react";
 import { View, Text, StyleSheet, FlatList, Image, TouchableOpacity } from "react-native";
-
+import { Feather } from '@expo/vector-icons';
 
 const DefaultScreenPosts = ({ route, navigation }) => {
 
@@ -16,27 +16,36 @@ const DefaultScreenPosts = ({ route, navigation }) => {
 
     return (
         <View style={styles.container}>
-        <FlatList
-            data={posts}
-            keyExtractor={(item, indx) => indx.toString()}
-            renderItem={({ item }) => (
-            <View
-                style={{
-                marginBottom: 10,
-                justifyContent: "center",
-                alignItems: "center",
-                }}
-            >
-                <Image
-                source={{ uri: item.photo }}
-                style={{ width: 343, height: 240, borderRadius: 8, }}
-                />
-            </View>
-            )}
-        />
-        <TouchableOpacity title="Location" onPress={() => navigation.navigate("MapScreen")} />
-        <TouchableOpacity
-            title="Comments" onPress={() => navigation.navigate("CommentsScreen")} />
+            <FlatList
+                data={posts}
+                keyExtractor={(item, indx) => indx.toString()}
+                renderItem={({ item }) => (
+                <View
+                    style={{
+                    marginBottom: 10,
+                    justifyContent: "center",
+                    alignItems: "center",
+                    }}
+                >
+                    <Image
+                    source={{ uri: item.photo }}
+                    style={{ width: 343, height: 240, borderRadius: 8, }}
+                    />
+                    <View  style={{ marginTop: 8, width: "100%", marginLeft: 8, marginRight: 8, justifyContent: "space-evenly"}}>
+                        <Text title="Name" style={{ fontSize: 16,  fontWeight: 'bold'}}>Name</Text>
+                        <View style={{flexDirection: 'row', alignItems: 'center',  marginTop: 11,  justifyContent: 'space-between'}}>
+                            <TouchableOpacity onPress={() => navigation.navigate("MapScreen")}> 
+                                <Feather name="message-circle" size={18} color="#BDBDBD" />
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={() => navigation.navigate("CommentsScreen") } style={{flexDirection: 'row', alignItems: 'center'}} > 
+                                <Feather name="map-pin" size={18} color="#BDBDBD" style={{marginRight: 6}}/>
+                                <Text style={{ fontSize: 16, marginLeft: 7, textDecorationLine: 'underline' }} >Location</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+                )}
+            />
         </View>
     );
 };
